b02f5567e5734e9f898985e0db0ddde4333dba3c
Brett Tolbert <bretttolbert@gmail.com>
2025-11-02 15:19:27 -0600

    refactoring

--- mediaserver.py
+++ mediaserver.py
@@ -4,7 +4,7 @@ import yaml
 from datetime import datetime
 import os.path
 import random  # type: ignore
-from typing import Dict, List, Set, Tuple, Union
+from typing import cast, Dict, List, Set, Tuple, Union
 from urllib.parse import quote_plus  # type: ignore
 from enum import StrEnum
 
@@ -21,7 +21,11 @@ MEDIASCAN_FILES_PATH = "../mediascan/out/files.yaml"
 files: MediaFiles = load_files_yaml(MEDIASCAN_FILES_PATH)
 
 
-class ArgTypeScalarInt(StrEnum):
+class ArgType(StrEnum):
+    pass
+
+
+class ArgTypeScalarInt(ArgType):
     MinYear = "minYear"
     MaxYear = "maxYear"
 
@@ -30,7 +34,7 @@ class ArgTypeScalar:
     Int = ArgTypeScalarInt
 
 
-class ArgTypeListStr(StrEnum):
+class ArgTypeListStr(ArgType):
     Genre = "genre"
     Artist = "artist"
     AlbumArtist = "albumartist"
@@ -43,7 +47,7 @@ class ArgTypeList:
     Str = ArgTypeListStr
 
 
-class ArgType:
+class ArgTypes:
     List = ArgTypeList
     Scalar = ArgTypeScalar
 
@@ -51,7 +55,7 @@ class ArgType:
 class ArgTypeUtil:
     @classmethod
     def is_scalar(cls, arg_type: ArgType) -> bool:
-        return arg_type in (ArgType.Scalar.Int.MinYear, ArgType.Scalar.Int.MaxYear)
+        return arg_type in (ArgTypes.Scalar.Int.MinYear, ArgTypes.Scalar.Int.MaxYear)
 
 
 ArgValueScalarInt = int
@@ -60,14 +64,14 @@ ArgValue = Union[ArgValueScalarInt, ArgValueListStr]
 ArgsDict = Dict[ArgType, ArgValue]
 
 REQUEST_ARG_TYPES = [
-    ArgType.List.Str.Genre,
-    ArgType.List.Str.Artist,
-    ArgType.List.Str.AlbumArtist,
-    ArgType.List.Str.Album,
-    ArgType.List.Str.Title,
-    ArgType.List.Str.Year,
-    ArgType.Scalar.Int.MinYear,
-    ArgType.Scalar.Int.MaxYear,
+    ArgTypes.List.Str.Genre,
+    ArgTypes.List.Str.Artist,
+    ArgTypes.List.Str.AlbumArtist,
+    ArgTypes.List.Str.Album,
+    ArgTypes.List.Str.Title,
+    ArgTypes.List.Str.Year,
+    ArgTypes.Scalar.Int.MinYear,
+    ArgTypes.Scalar.Int.MaxYear,
 ]
 
 
@@ -77,54 +81,45 @@ def get_request_args(
     ret: ArgsDict = {}
     for arg_type in arg_types:
         if ArgTypeUtil.is_scalar(arg_type):
-            ret[arg_type] = request.args.get(arg_type)
+            value = request.args.get(str(arg_type))
+            if value:
+                ret[arg_type] = int(value)
         else:
-            ret[arg_type] = request.args.getlist(arg_type)
+            value = request.args.getlist(str(arg_type))
+            if value:
+                ret[arg_type] = value
     return ret
 
 
 def filter_files(files: MediaFiles, args: ArgsDict) -> List[MediaFile]:
     ret: List[MediaFile] = []
     for f in files.files:
-        if (
-            len(args[ArgType.List.Str.Artist])
-            and f.artist not in args[ArgType.List.Str.Artist]
-        ):
-            continue
-        if (
-            len(args[ArgType.List.Str.AlbumArtist])
-            and f.albumartist not in args[ArgType.List.Str.AlbumArtist]
-        ):
-            continue
-        if (
-            len(args[ArgType.List.Str.Album])
-            and f.album not in args[ArgType.List.Str.Album]
-        ):
-            continue
-        if (
-            len(args[ArgType.List.Str.Genre])
-            and f.genre not in args[ArgType.List.Str.Genre]
-        ):
-            continue
-        if (
-            len(args[ArgType.List.Str.Title])
-            and f.title not in args[ArgType.List.Str.Title]
-        ):
-            continue
-        if (
-            len(args[ArgType.List.Str.Year])
-            and str(f.year) not in args[ArgType.List.Str.Year]
-        ):
-            continue
-        if args[ArgType.Scalar.Int.MinYear] and f.year < int(
-            args[ArgType.Scalar.Int.MinYear]
-        ):
-            continue
-        if args[ArgType.Scalar.Int.MaxYear] and f.year > int(
-            args[ArgType.Scalar.Int.MaxYear]
-        ):
-            continue
-        ret.append(f)
+        # ArgTypeListStr:
+        arg_type_list_file_value_map: Dict[ArgType, str] = {
+            ArgTypes.List.Str.Artist: f.artist,
+            ArgTypes.List.Str.AlbumArtist: f.albumartist,
+            ArgTypes.List.Str.Album: f.album,
+            ArgTypes.List.Str.Genre: f.genre,
+            ArgTypes.List.Str.Title: f.title,
+            ArgTypes.List.Str.Year: str(f.year),
+        }
+        for arg_type, file_value in arg_type_list_file_value_map.items():
+            if arg_type in args:
+                arg_value_list = cast(ArgValueListStr, args[arg_type])
+                if len(arg_value_list) and file_value not in arg_value_list:
+                    continue
+        # ArgTypeScalarInt:
+        arg_type = ArgTypes.Scalar.Int.MinYear
+        if arg_type in args:
+            arg_value = cast(ArgValueScalarInt, args[arg_type])
+            if arg_value and f.year < arg_value:
+                continue
+        arg_type = ArgTypes.Scalar.Int.MaxYear
+        if arg_type in args:
+            arg_value = cast(ArgValueScalarInt, args[arg_type])
+            if arg_value and f.year > arg_value:
+                continue
+            ret.append(f)
     return ret
 
 
@@ -268,7 +263,10 @@ def send_report(path: str) -> None:
 
 @app.route("/genres")  # type: ignore
 def genre_counts() -> None:
-    sort: str = request.args.get("sort")  # type: ignore
+    sort: str = ""
+    value = request.args.get("sort")
+    if value:
+        sort = value
     return render_template(
         "genres.html",
         genre_counts=get_genre_counts(files, sort=sort),  # type: ignore
@@ -278,7 +276,10 @@ def genre_counts() -> None:
 @app.route("/artists")  # type: ignore
 def artist_counts() -> None:
     genres: List[str] = request.args.getlist("genre")  # type: ignore
-    sort: str = request.args.get("sort")  # type: ignore
+    sort: str = ""
+    value = request.args.get("sort")
+    if value:
+        sort = value
     return render_template(
         "artists.html",
         artist_counts=get_artist_counts(files, filter_genres=genres, sort=sort),  # type: ignore
