83bbf5d9870c268628330442562d2bd915c1d6ed
Brett Tolbert <bretttolbert@gmail.com>
2025-11-30 17:20:16 -0600

    bugfix

--- app/utils/flask_app_utils.py
+++ app/utils/flask_app_utils.py
@@ -1,9 +1,10 @@
 from datetime import datetime
+from urllib.parse import quote_plus
 from flask import Flask
 
 
 def configure_flask_app(app: Flask):
-    app.jinja_env.filters["quote_plus"] = lambda u: quote_plus(u)  # type: ignore
-    app.jinja_env.filters["make_list"] = lambda s: list(s)  # type: ignore
-    app.jinja_env.globals["PRESENT_YEAR"] = datetime.now().year  # type: ignore
+    app.jinja_env.filters["quote_plus"] = lambda u: quote_plus(u)
+    app.jinja_env.filters["make_list"] = lambda s: list(s)
+    app.jinja_env.globals["PRESENT_YEAR"] = datetime.now().year
     return app
