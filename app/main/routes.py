bf17223e207a071426f08330f7729f01f91ca432
Brett Tolbert <bretttolbert@gmail.com>
2025-12-05 15:01:57 -0600

    config wip

--- app/main/routes.py
+++ app/main/routes.py
@@ -3,18 +3,20 @@ from flask import (
     current_app,
     render_template,
     request,
-    Response,
     send_from_directory,
+    Flask,
+    Response,
 )
 import random
-from typing import List
+from typing import cast, List, Optional
 from pathlib import Path
 
 from mediascan import load_files_yaml, MediaFiles, MediaFile
 
 from app.main import bp
-
 from app.types.arg_types import args_dict_to_str
+from app.types.config.mediaserver_config import MediaServerConfig
+from app.utils.config.mediaserver_config_util import MediaServerConfigUtil
 from app.utils.request_args_utils import get_request_args
 from app.utils.media_files_utils import (
     filter_files,
@@ -25,11 +27,25 @@ from app.utils.media_files_utils import (
     get_genre_counts,
     get_artist_urls,
 )
-from app.utils.config.mediaserver_config_util import MediaServerConfigUtil
 
-config = MediaServerConfigUtil().load_config()
+_default_config = MediaServerConfigUtil().load_config()
 
-files: MediaFiles = load_files_yaml(config.files_yaml_path)
+
+def get_config(app: Flask):
+    if app.config["MEDIASERVER_CONFIG"] is not None:
+        return cast(MediaServerConfig, current_app.config["MEDIASERVER_CONFIG"])
+    else:
+        return _default_config
+
+
+_files: Optional[MediaFiles] = None
+
+
+def get_files(config: MediaServerConfig):
+    global _files
+    if _files is None:
+        _files = load_files_yaml(config.files_yaml_path)
+    return _files
 
 
 @bp.route("/")
@@ -44,7 +60,9 @@ def tracks() -> str:
     global files
     args = get_request_args(request)
     current_app.logger.debug("tracks args=%s", args_dict_to_str(args))
-    files_list: List[MediaFile] = filter_files(current_app, files, args)
+    files_list: List[MediaFile] = filter_files(
+        current_app, get_files(get_config(current_app)), args
+    )
     cover_path: Path = Path()
     if len(files_list):
         cover_path = get_cover_path(files_list[0])
@@ -68,7 +86,9 @@ def tracks_index() -> str:
 def player() -> str:
     return render_template(
         "player.html",
-        search_query_url_format=config.playback_methods.youtube.search_query_url_format,
+        search_query_url_format=get_config(
+            current_app
+        ).playback_methods.youtube.search_query_url_format,
     )
 
 
@@ -84,6 +104,7 @@ def name_that_tune_index() -> str:
 
 @bp.route("/getfile/<path:path>")
 def send_report(path: str) -> Response:
+    config = get_config(current_app)
     if not path.startswith("/"):
         path = "/" + path
     path_prefix = config.playback_methods.local.media_path
@@ -114,7 +135,7 @@ def genres() -> str:
         sort = value
     return render_template(
         "genres.html",
-        genre_counts=get_genre_counts(files, sort=sort),
+        genre_counts=get_genre_counts(get_files(get_config(current_app)), sort=sort),
     )
 
 
@@ -134,7 +155,10 @@ def artist_counts() -> str:
     return render_template(
         "artists.html",
         artist_counts=get_artist_counts(
-            current_app, files, get_request_args(request), sort=sort
+            current_app,
+            get_files(get_config(current_app)),
+            get_request_args(request),
+            sort=sort,
         ),
     )
 
@@ -152,7 +176,11 @@ def albums() -> str:
         "albums.html",
         albums=[
             album.to_tuple()
-            for album in get_albums(current_app, files, get_request_args(request))
+            for album in get_albums(
+                current_app,
+                get_files(get_config(current_app)),
+                get_request_args(request),
+            )
         ],
     )
 
@@ -168,7 +196,7 @@ def albums_index() -> str:
 def genres_cloud() -> str:
     return render_template(
         "word-cloud.html",
-        word_urls=get_genre_urls(files),
+        word_urls=get_genre_urls(get_files(get_config(current_app))),
     )
 
 
@@ -176,7 +204,9 @@ def genres_cloud() -> str:
 def artists_cloud() -> str:
     return render_template(
         "word-cloud.html",
-        word_urls=get_artist_urls(current_app, files, get_request_args(request)),
+        word_urls=get_artist_urls(
+            current_app, get_files(get_config(current_app)), get_request_args(request)
+        ),
     )
 
 
@@ -185,7 +215,9 @@ def api_track():
     global files
     args = get_request_args(request)
     current_app.logger.debug("api/track args=%s", args_dict_to_str(args))
-    files_list: List[MediaFile] = filter_files(current_app, files, args)
+    files_list: List[MediaFile] = filter_files(
+        current_app, get_files(get_config(current_app)), args
+    )
     if not len(files_list):
         abort(404)
     file = random.choice(files_list)
