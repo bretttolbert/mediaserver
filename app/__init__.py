9e36874ddd86116790a9fe87f1ffa2a97618c075
Brett Tolbert <bretttolbert@gmail.com>
2025-12-05 14:47:44 -0600

    config wip

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ from flask import Flask
 
 def format_search_query_url(args: Dict[str, str], config: MediaServerConfig):
     artist = quote_plus(args["artist"])
-    album = quote_plus(args["albumm"])
+    album = quote_plus(args["album"])
     title = quote_plus(args["title"])
     ret = config.playback_methods.youtube.search_query_url_format
     ret = ret.replace("{artist}", artist)
@@ -25,7 +25,9 @@ def create_app(config: MediaServerConfig):
     app.debug = config.debug
     app.jinja_env.filters["quote_plus"] = lambda u: quote_plus(u)
     app.jinja_env.filters["make_list"] = lambda s: list(s)
-    app.jinja_env.filters["format_search_query_url"] = lambda s: s
+    app.jinja_env.filters["format_search_query_url"] = (
+        lambda args: format_search_query_url(args, config)
+    )
     app.jinja_env.globals["PRESENT_YEAR"] = datetime.now().year
     from app.main import bp as main_bp
 
