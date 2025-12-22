263cd06f895b6364267ce297ffb843b182632829
Brett Tolbert <bretttolbert@gmail.com>
2025-12-21 18:36:29 -0600

    fixed year shuffle link, misc

--- dev/scripts/upload_mediaserver.sh
+++ dev/scripts/upload_mediaserver.sh
@@ -7,7 +7,7 @@ SVC=mediaserver
 SOURCE=$SVC/
 DEST=$DEST_USER@$HOST:$DEST_ROOT/$SVC/
 echo "Uploading $SOURCE to $DEST"
-# If first run, remove '--exclude 'app/templates/_css.html'
+# If first run, remove '--exclude 'app/templates/css.html'
 # I added that because I've put my analytics html there
-rsync -ahvP $SOURCE $DEST --delete --exclude 'app/templates/_css.html' --exclude 'dev/' --exclude mediaserver_config.yaml --exclude '.git/' --exclude '.gitignore' --exclude '__pycache__/'
+rsync -ahvP $SOURCE $DEST --delete --exclude 'app/templates/css.html' --exclude 'dev/' --exclude mediaserver_config.yaml --exclude '.git/' --exclude '.gitignore' --exclude '__pycache__/'
 echo "Uploaded $SOURCE to $DEST"
